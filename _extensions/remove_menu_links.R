12f67c89dad15c6ddfbb0b3c4eb0f51380fcd48c
sda030 <stephus.daus@gmail.com>
2024-09-12 17:27:48 +0200

    V1.1

--- _extensions/remove_menu_links.R
+++ _extensions/remove_menu_links.R
@@ -1,7 +1,13 @@
 fs::dir_ls(path = "_site", type = "file", regexp = "\\.html", recurse = TRUE) |>
-    purrr::map_chr(.f = ~ {
+    stringr::str_subset(pattern = "-NIFU-R91188BK\\.html") |>
+    fs::file_delete()
+
+Sys.sleep(30)
+fs::dir_ls(path = "_site", type = "file", regexp = "\\.html", recurse = TRUE) |>
+    purrr::walk(.f = ~ {
         readr::read_file(.x) |>
-            stringr::str_replace_all(pattern = "<li>\n.+\n.+Epost:.+\n +</li> +\n", replacement = "") |>
+            stringr::str_replace_all(pattern = "<li>\n.+\n.+Epost:.+\n *</li> *\n", replacement = "") |>
             stringr::str_replace_all(pattern = '<li class="sidebar-item">\n.+\n.+\n.+Epost:.+\n.+\n</li>\n', replacement = "") |>
             readr::write_file(.x)
     })
+cli::cli_inform("Done post-processing!")
