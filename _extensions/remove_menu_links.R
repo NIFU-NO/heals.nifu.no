8b704e7960b3a4ba6616ad2bfa33b37dc78bc9ec
Stephan Daus <stephus.daus@gmail.com>
2024-11-09 19:33:32 +0100

    V1.4

--- _extensions/remove_menu_links.R
+++ _extensions/remove_menu_links.R
@@ -1,13 +1,24 @@
-fs::dir_ls(path = "_site", type = "file", regexp = "\\.html", recurse = TRUE) |>
+Sys.sleep(30)
+dirpath <-
+    paste(c("_site", stringi::stri_remove_empty(Sys.getenv("QUARTO_PROFILE"))),
+        collapse = "-"
+    )
+
+
+dirpath |>
+    fs::dir_ls(
+        path = _,
+        type = "file", regexp = "\\.html", recurse = TRUE
+    ) |>
     stringr::str_subset(pattern = "-NIFU-R91188BK.*\\.html") |>
     fs::file_delete()
 
-Sys.sleep(30)
-fs::dir_ls(path = "_site", type = "file", regexp = "\\.html", recurse = TRUE) |>
+dirpath |>
+    fs::dir_ls(path = _, type = "file", regexp = "\\.html", recurse = TRUE) |>
     purrr::walk(.f = ~ {
         readr::read_file(.x) |>
             stringr::str_replace_all(pattern = "<li>\n.+\n.+Epost:.+\n *</li> *\n", replacement = "") |>
             stringr::str_replace_all(pattern = '<li class="sidebar-item">\n.+\n.+\n.+Epost:.+\n.+\n</li>\n', replacement = "") |>
             readr::write_file(.x)
     })
-cli::cli_inform("Done post-processing!")
+cli::cli_inform("Done post-processing at {format(Sys.time(), '%X')}!")
